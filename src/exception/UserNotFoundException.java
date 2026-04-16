be2b90abff71f697e62a0560796af086b1f3e5e2
thuhahathu <25023407@vnu.edu.vn>
2026-04-17 01:44:22 +0700

    add exception

--- src/exception/UserNotFoundException.java
+++ src/exception/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package exception;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends Exception{
+    private final String keyword;
+    public UserNotFoundException(String keyword) {
+        super("Không tìm thấy người dùng: '" + keyword + "'");
+        this.keyword = keyword;
+    }
+    public String getKeyword() {
+        return keyword;
+    }
 }
